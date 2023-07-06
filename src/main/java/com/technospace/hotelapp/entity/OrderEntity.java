eb09c25c8f68d23a043c2cc4720872b6370d40ac
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-07-06 15:23:27 +0530

    [MODIFIED]:[Janhavi] Creating Customer and Order Entity Classes

---
+++ src/main/java/com/technospace/hotelapp/entity/OrderEntity.java
@@ -0,0 +1,36 @@
+package com.technospace.hotelapp.entity;
+
+public class OrderEntity {
+
+	private int oid;
+	private int cid;
+	private String odate;
+	private int oamount;
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getOdate() {
+		return odate;
+	}
+	public void setOdate(String odate) {
+		this.odate = odate;
+	}
+	public int getOamount() {
+		return oamount;
+	}
+	public void setOamount(int oamount) {
+		this.oamount = oamount;
+	}
+	
+	
+	
+}
