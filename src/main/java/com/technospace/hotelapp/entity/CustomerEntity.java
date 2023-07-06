c2b7db475076e2c65ded4bf2e85e4a05aa7319ac
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-07-06 15:07:12 +0530

    [ADDED]:[Janhavi] Welcome Message for Hotel App Home Page

---
+++ src/main/java/com/technospace/hotelapp/entity/CustomerEntity.java
@@ -0,0 +1,36 @@
+package com.technospace.hotelapp.entity;
+
+public class CustomerEntity {
+
+	private int cid;
+	private String cname;
+	private String mobno;
+	private String emailid;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getMobno() {
+		return mobno;
+	}
+	public void setMobno(String mobno) {
+		this.mobno = mobno;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	
+	
+	
+}
