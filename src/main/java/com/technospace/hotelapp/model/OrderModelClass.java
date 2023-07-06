a4c0d829243f02736f21dd88990b642a9dd65a9e
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-07-06 15:42:11 +0530

    [MODIFIED]:[Janhavi] Creating Model Package For Order Class

---
+++ src/main/java/com/technospace/hotelapp/model/OrderModelClass.java
@@ -0,0 +1,33 @@
+package com.technospace.hotelapp.model;
+
+public class OrderModelClass {
+
+	private int oid;
+	private int cid;
+	private String odate;
+	private int oamount;
+	public int getOid() {
+		return oid;
+	}
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getOdate() {
+		return odate;
+	}
+	public void setOdate(String odate) {
+		this.odate = odate;
+	}
+	public int getOamount() {
+		return oamount;
+	}
+	public void setOamount(int oamount) {
+		this.oamount = oamount;
+	}
+}
